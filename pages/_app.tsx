53529ec2da013906268214d7e1fe0b886497183c
Juned-H2 <juned.hussa@outlook.com>
2022-05-14 01:50:44 +0100

    add css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import theme from '../theme';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
   );
